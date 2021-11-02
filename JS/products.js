81197e71935e89d2712ac8a9acfd17f80c977011
adrianmcenturion <adrianmcenturion@gmail.com>
2021-11-02 17:38:04 -0300

    nav agregada

--- JS/products.js
+++ JS/products.js
@@ -1,6 +1,15 @@
 const mangaURL = 'https://kitsu.io/api/edge/manga?page[limit]=20&page[offset]=0&sort=ratingRank'
+const searchMangaURL = 'https://kitsu.io/api/edge/manga'
 
 const containerMangas = document.getElementById('container-mangas')
+const btnBuscar = document.getElementById('btnBuscar')
+const inputBuscar = document.getElementById('inputBuscar')
+
+btnBuscar.addEventListener('click', searchMangas)
+
+
+
+
 
 
 
@@ -25,9 +34,9 @@ const renderMangas = (mangas) => {
     mangas.forEach(manga => {
          const containerCards = document.createElement('div');
          containerCards.className = ('containerCards')
-         containerCards.classList.add('col-12', 'col-sm-6', 'col-lg-3', 'mb-4')
+         
          const containerCard = document.createElement('div');
-         containerCard.className = ('containerCard', 'card')
+         containerCard.className = ('containerCard')
          
          const img = document.createElement('img');
          img.src = manga.attributes.posterImage.medium;
@@ -46,12 +55,12 @@ const renderMangas = (mangas) => {
 
          const btnComprar = document.createElement('a')
          btnComprar.className = ('btn')
-         btnComprar.classList.add('btn-sm', 'btn-primary')
+         
          btnComprar.innerText = "Comprar"
          
          const btnDetalles = document.createElement('a')
          btnDetalles.className = ('btn')
-         btnDetalles.classList.add('btn-sm', 'btn-secondary')
+         
          btnDetalles.innerText = "Detalles"
 
          
@@ -67,6 +76,11 @@ const renderMangas = (mangas) => {
          containerCards.appendChild(containerCard);
          containerMangas.appendChild(containerCards)
 
+         containerCards.classList.add('col-12', 'col-sm-6', 'col-lg-3', 'mb-4')
+         containerCard.classList.add('card')
+         btnComprar.classList.add('btn-sm', 'btn-primary')
+         btnDetalles.classList.add('btn-sm', 'btn-secondary')
+
          
          
      });
@@ -75,3 +89,29 @@ const renderMangas = (mangas) => {
  }
 
 getMangas()
+
+function searchMangas () {
+
+    const getMangas = async () => {
+        try {
+    
+            const response = await fetch(searchMangaURL, {
+                method: 'GET'
+            });
+    
+            const json = await response.json();
+            const { data } = json;
+            renderMangas(data);
+            console.log(data)
+            console.log(data.attributes)
+            
+        } catch( error ) {
+            alert(error);
+        }
+        
+        
+        
+    };
+    alert('asdasd')
+    
+}
